c422db9ef12493762b892a990c442ad7c5ee5a0b
VictorBarros <victorbarros1130@gmail.com>
2021-01-28 17:43:35 -0300

    fix query and add log

--- subscription-manager/database/transaction.go
+++ subscription-manager/database/transaction.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/sirupsen/logrus"
 )
 
 // InsertTransaction to Database
 func (db *Database) InsertTransaction(id string, createdAt int) error {
-
 	var query string = fmt.Sprintf(
-		"INSERT INTO transaction VALUES (%s, %d);", id, createdAt,
+		"INSERT INTO transaction VALUES ('%s', FROM_UNIXTIME(%d));", id, createdAt,
 	)
 	return db.executeQuery(query)
 }
@@ -19,7 +19,9 @@ func (db *Database) executeQuery(query string) error {
 	var ids []int
 	err := db.Connection.Select(&ids, query)
 	if err != nil {
+		logrus.WithError(err).Errorf("fail to execute query %s", query)
 		return err
 	}
-	return err
+
+	return nil
 }
