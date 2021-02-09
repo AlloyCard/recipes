551340fd80728d8764bb5fd95a59ed7eae1c1790
VictorBarros <victorbarros1130@gmail.com>
2021-02-09 13:07:57 -0300

    add jwt service

--- subscription-manager/internal/jwt/jwt.go
+++ subscription-manager/internal/jwt/jwt.go
@@ -1,40 +1,26 @@
 package jwt
 
 import (
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"time"
 
-	"github.com/aws/aws-sdk-go/service/kms"
+	jwt "github.com/dgrijalva/jwt-go"
+	helper "github.com/dgrijalva/jwt-go/test"
 )
 
-func BuildRecipeToken(recipeId string, keyId string) {
-	header := map[string]string{
-		"alg": "RS256",
-		"typ": "JWT",
-		"kid": fmt.Sprintf(`AlloyPrincipal-%s`, recipeId),
-	}
-	payload := map[string]interface{}{
+// BuildJWT buildwith private key
+func BuildJWT(recipeID string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
 		"exp":                  time.Now().UTC().Add(time.Second * 60).UnixNano(),
 		"iat":                  time.Now().UTC().UnixNano(),
 		"iss":                  "AlloyCard",
-		"custom:principalId":   recipeId,
+		"custom:principalId":   recipeID,
 		"custom:principalType": "com.alloycard.core.entities.recipe.Recipe",
-	}
+	})
+	token.Header["kid"] = fmt.Sprintf(`AlloyPrincipal-%s`, recipeID)
 
-	headerMarshal, _ := json.Marshal(header)
-	// TODO log err
-	headerEncoded := base64.StdEncoding.EncodeToString(headerMarshal)
+	// TODO move pem's path to env
+	privateKey := helper.LoadRSAPrivateKeyFromDisk("./jwtRSA256-private.pem")
 
-	payloadMarshal, _ := json.Marshal(payload)
-	// TODO log err
-	payloadEncoded := base64.StdEncoding.EncodeToString(payloadMarshal)
-
-	input := kms.SignInput{
-		KeyId: &keyId,
-		// Message: []byte(message),
-		MessageType:      "RAW",
-		SigningAlgorithm: "RSASSA_PKCS1_V1_5_SHA_256",
-	}
+	return token.SignedString(privateKey)
 }
